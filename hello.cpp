f85404993c42d9be70afb6249b872cb5ae822e91
CarlosLaraFP <info@carloslaraai.com>
2023-07-03 10:24:06 -0700

    Setup and immutable references loop

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+// g++ -std=c++17 hello.cpp -o hello
 int main() {
-    std::cout << "Hello world!\n";
+  std::vector<std::string> colors{"Blue", "Green", "Red"};
+
+  // Iterating as immutable references
+  for (std::string const &color : colors) {
+    std::cout << "Hello " << color << "!\n";
+  }
 }
